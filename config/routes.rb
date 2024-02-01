665c6fcbd182b46459e7ba203d872c3f6a54ab9b
supenser <fenders390@gmail.com>
2024-02-01 15:11:23 +0900

    deviseコントローラー・ビュー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
-  devise_for :users
-  devise_for :admins
+  devise_for :users, controllers: {
+    registrations: "users/registrations",
+    sessions: "users/sessions",
+    passwords: "users/passwords",
+    confirmations: "users/confirmations"
+  }
+
+  devise_for :admins, controllers: {
+    registrations: "admins/registrations",
+    sessions: "admins/sessions",
+    passwords: "admins/passwords",
+    confirmations: "admins/confirmations"
+  }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
