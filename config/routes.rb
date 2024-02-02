9ca247c38eae21af903225725af77c1fd96eb279
supenser <fenders390@gmail.com>
2024-02-02 18:22:39 +0900

    ルーティング設定・トップページ指定

--- config/routes.rb
+++ config/routes.rb
@@ -12,8 +12,7 @@ Rails.application.routes.draw do
     passwords: "admins/passwords",
     confirmations: "admins/confirmations"
   }
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root to: 'items#index'
+
 end
