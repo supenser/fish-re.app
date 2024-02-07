08f188c049adbe28e612f4a244565df6d499c2b7
supenser <fenders390@gmail.com>
2024-02-08 04:03:07 +0900

    バリデーション設定・コントローラー調整

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -2,8 +2,20 @@ class ReservationsController < ApplicationController
 
   def index
     @items = Item.all.order("created_at DESC")
+    @reservation = Reservation.new
   end
 
-  
+  def create
+    @reservation = Reservation.new(reservation_params)
+  end
+
+
+  private
+
+  def reservation_params
+    params.require(:reservation).permit( :reserve_id, :fish_recipe_id, :telephone ).merge(user_id: current_user.id, item_id:params[:item_id])
+  end
+
+
 
 end
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,6 +1,11 @@
 class Reservation < ApplicationRecord
 
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :reserve
+  belongs_to :fish_recipe
 
-
+  validates :reserve_id, :fish_recipe_id, :telephone, presence: true
+  validates :reserve_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :fish_recipe_id, numericality: { other_than: 1, message: "can't be blank" }
   
 end
